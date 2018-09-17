2a68d5965e0f08f692b4d143b8313b23c5a31670
Natnael Alemayehu <dinksira@gmail.com>
2018-09-17 01:59:26 -0500

    Update FileLoader.java

--- src/com/metrostate/ics460/project1/sender/FileLoader.java
+++ src/com/metrostate/ics460/project1/sender/FileLoader.java
@@ -1,7 +1,39 @@
 package com.metrostate.ics460.project1.sender;
 
-public interface FileLoader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public byte[] loadFile();
-	
+import javax.swing.JFileChooser;
+
+public class FileLoader {
+	private JFileChooser file_Chooser;
+	private BufferedReader bufferedReader;
+	private FileReader fileReader;
+
+	public void loadFile() {
+	String fileName;
+		file_Chooser = new JFileChooser();
+		file_Chooser.setCurrentDirectory(new File("."));
+		if (file_Chooser.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
+			
+			try {
+				 fileName = file_Chooser.getSelectedFile().getName();
+				fileReader = new FileReader(fileName);
+				bufferedReader = new BufferedReader(fileReader);
+				String line;
+				
+				while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(fileName);
+					System.out.println(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			System.out.println("oops ... File not exist");
+		}
+	}
+
 }
