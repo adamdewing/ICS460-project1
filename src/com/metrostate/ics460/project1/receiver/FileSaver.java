bd7da839f33b551e50475e459009494bd32f0986
Natnael Alemayehu <dinksira@gmail.com>
2018-09-21 02:50:57 -0500

    Update FileSaver.java

--- src/com/metrostate/ics460/project1/receiver/FileSaver.java
+++ src/com/metrostate/ics460/project1/receiver/FileSaver.java
@@ -1,34 +1,39 @@
 package com.metrostate.ics460.project1.receiver;
 
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileSaver {
+import javax.swing.JFileChooser;
 
-	private File file;
-	private StringBuilder string_builder;
-	private FileWriter file_writer;
-	private BufferedWriter buffered_writer;
-	private PrintWriter print_writer;
+public class FileSaver implements FileProcessor {
 
-	public void saveFile(String str) {
-		file = new File( "output.txt" );
-		string_builder.append(str);
-		String data = string_builder.toString();
-		
+	private List<byte[]> lines = new ArrayList<byte[]>();
+
+	public void saveFile(String data) {
+		File file = new File( "output.txt" );
+
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(data);
+
+		FileWriter fileWriter;
 		try {
-			file_writer = new FileWriter(file, true);
-			buffered_writer = new BufferedWriter(file_writer);
-			print_writer = new PrintWriter(buffered_writer);
-			print_writer.println(data);
-			print_writer.close();
-
+			fileWriter = new FileWriter(file, true);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			PrintWriter printWriter = new PrintWriter(bufferedWriter);
+			printWriter.println(stringBuilder.toString());
+			printWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+}
 
 }
