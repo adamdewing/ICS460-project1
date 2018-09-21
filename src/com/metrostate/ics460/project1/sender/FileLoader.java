80b64200c3af2dfc64ab4399ea94675777fc49d0
Natnael Alemayehu <dinksira@gmail.com>
2018-09-21 02:44:27 -0500

    Update FileLoader.java

--- src/com/metrostate/ics460/project1/sender/FileLoader.java
+++ src/com/metrostate/ics460/project1/sender/FileLoader.java
@@ -1,42 +1,46 @@
 package com.metrostate.ics460.project1.sender;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 
-public class FileLoader {
-	private JFileChooser file_Chooser;
-	private BufferedReader bufferedReader;
-	private FileReader fileReader;
-	private byte[] sendData;
-    
-	public byte[] loadFile() {
-		String fileName;
-		file_Chooser = new JFileChooser();
+public class FileLoader implements FileProcessor {
+
+	private String fileName;
+	private String line;
+	private List<byte[]> lines = new ArrayList<byte[]>();
+
+	public List<byte[]> loadFile() {
+		JFileChooser file_Chooser = new JFileChooser();
 		file_Chooser.setCurrentDirectory(new File("."));
-		if (file_Chooser.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
+		if (file_Chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			fileName = file_Chooser.getSelectedFile().getName();
 
 			try {
-				fileName = file_Chooser.getSelectedFile().getName();
-				fileReader = new FileReader(fileName);
-				bufferedReader = new BufferedReader(fileReader);
-				String line;
+				FileReader fileReader = new FileReader(new File(fileName));
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+
 
 				while ((line = bufferedReader.readLine()) != null) {
-					sendData = line.getBytes("US-ASCII");
+					//convert the String input into the byte array.
+					lines.add(line.getBytes("US-ASCII"));
+					//setLines(line.getBytes("US-ASCII"));	
 				}
+
+				fileReader.close();
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}else {
-			System.out.println("oops ... File not exist");
 		}
-		
-		return sendData;
+		return lines;
+
 	}
-
-}
-
