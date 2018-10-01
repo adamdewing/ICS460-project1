892f02685ee89b84029a4479aa92d416b3bb9bde
Natnael Alemayehu <dinksira@gmail.com>
2018-10-01 00:48:12 -0500

    Update FileProcessor.java

--- src/com/metrostate/ics460/project1/FileProcessor.java
+++ src/com/metrostate/ics460/project1/FileProcessor.java
@@ -2,10 +2,32 @@ package com.metrostate.ics460.project1;
 
 import java.util.List;
 
-public interface FileProcessor {
+/**
+ * Course: 		ICS 460 Networks And Security
+ * Project Description: A project uses UDP protocol to send and a receive a binary file.
+ * 			The sender accepts a file as a command line parameter (any binary 
+ * 			file on your hard disk), breaks it into smaller chunks.
+ * Class Description: 	An interface to read and save a file onto disk
+ * Instructor: 		Professor Demodar Chetty
+ * @author Natnael Alemayehu
+ *
+ */
 
-	public List<byte[]> loadFile();
+/**
+ * An interface to read and save a file onto disk *
+ */
+public interface FileProcessor {
 	
-	public void saveFile(List<byte[]> byteList);
+	/**
+	 * A public method read a file from a disk
+	 * @return A list of byte array
+	 */
+	public List<byte[]> fileLoader();
+	
+	/**
+	 * A public method save a file to a disk
+	 * @param data
+	 */
+	public void fileSaver(String data);
 
 }
