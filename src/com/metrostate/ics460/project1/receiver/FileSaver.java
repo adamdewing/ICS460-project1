f1a41ac64f03a7f28291a857dc8d8a08a8ad3efa
Natnael Alemayehu <dinksira@gmail.com>
2018-10-02 00:41:18 -0500

    Update FileSaver.java

--- src/com/metrostate/ics460/project1/receiver/FileSaver.java
+++ src/com/metrostate/ics460/project1/receiver/FileSaver.java
@@ -10,10 +10,24 @@ import java.util.List;
 
 import com.metrostate.ics460.project1.FileProcessor;
 
+/**
+ * Course: 		ICS 460 Networks And Security
+ * Project Description: A project uses UDP protocol to send and a receive a binary file.
+ * 			The sender accepts a file as a command line parameter (any binary 
+ * 			file on your hard disk), breaks it into smaller chunks.
+ * Class Description: 	A class FileSaver implements a FileProcessor interface to read a file from a disk 
+ * 			and save a file onto disk
+ * Instructor: 		Professor Demodar Chetty
+ * @author Natnael Alemayehu
+ *
+ */
 public class FileSaver implements FileProcessor {
 
 	private List<byte[]> lines = new ArrayList<byte[]>();
 
+	/**
+	* A method save a file onto a disk it takes a list of byte aarys as an argument
+	*/
 	public void saveFile(List<byte[]> byteList) {
 		File file = new File("output.txt");
 		FileWriter fileWriter;
@@ -24,7 +38,9 @@ public class FileSaver implements FileProcessor {
 			for(byte[] bytes : byteList) {
 				printWriter.println(new String(bytes, "UTF-8"));
 			}
+			
 			printWriter.close();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
